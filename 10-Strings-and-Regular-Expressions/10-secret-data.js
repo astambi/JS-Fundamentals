b8fcc5cc472fcfe3d191b3e8774dcd88471d5e43
Anna <annsta@gmail.com>
2016-10-12 02:21:02 +0300

    Strings and Regular Expressions

--- 10-Strings-and-Regular-Expressions/10-secret-data.js
+++ 10-Strings-and-Regular-Expressions/10-secret-data.js
@@ -1,9 +1,27 @@
+// function secretData (arr) {
+//     let pattern = /(\*[A-Z][a-zA-Z]*)(?=[ \t]|$)|(\+[\d-]{10})(?=[ \t]|$)|(![a-zA-Z\d]+)(?=[ \t]|$)|(_[a-zA-Z\d]+)(?=[ \t]|$)/g;
+//     arr.forEach(line => {
+//         line = line.replace(pattern, match => '|'.repeat(match.length));
+//         console.log(line);
+//     });
+// }
+
 function secretData (arr) {
-    let pattern = /(\*[A-Z][a-zA-Z]*)(?=[ \t]|$)|(\+[\d-]{10})(?=[ \t]|$)|(![a-zA-Z\d]+)(?=[ \t]|$)|(_[a-zA-Z\d]+)(?=[ \t]|$)/g;
-    arr.forEach(line => {
-        line = line.replace(pattern, match => '|'.repeat(match.length));
+    for (let line of arr) {
+        let clientPattern = /(\*[A-Z][a-zA-Z]*)(?=[ \t]|$)/g;  // NB. replace with regexPattern, instead of string
+        let phonenumPattern = /(\+[\d-]{10})(?=[ \t]|$)/g;
+        let idPattern = /(![a-zA-Z\d]+)(?=[ \t]|$)/g;
+        let basePattern = /(_[a-zA-Z\d]+)(?=[ \t]|$)/g;
+        line = line.replace(clientPattern, maskText)
+                    .replace(phonenumPattern, maskText)
+                    .replace(idPattern, maskText)
+                    .replace(basePattern, maskText);
         console.log(line);
-    });
+        
+        function maskText(match) {
+            return '|'.repeat(match.length);
+        }
+    }
 }
 
 secretData([
